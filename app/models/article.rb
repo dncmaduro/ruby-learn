a805d0f5b8000964608624286a8ed89b8615bf73
gdschanu-manhdung <manhdungtran.vnuuet@gmail.com>
2024-04-10 01:21:36 +0700

    dependent when delete

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
     include Visible
     include Count
 
-    has_many :comments
+    has_many :comments, dependent: :destroy
 
     validates :title, presence: true, length: {maximum: 20}
     validates :body, presence: true, length: {minimum: 2}
