2083dd78a388ca9e35055a3b6a5ed357b9415b68
gdschanu-manhdung <manhdungtran.vnuuet@gmail.com>
2024-04-10 01:28:20 +0700

    basic authen

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "maduro", password: "623876", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
