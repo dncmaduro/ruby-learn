465f43e6bdd407e382fe5e22720f4d6ace04f11e
gdschanu-manhdung <manhdungtran.vnuuet@gmail.com>
2024-04-09 15:49:24 +0700

    Edit and Delete

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+    validates :title, presence: true, length: {maximum: 20}
+    validates :body, presence: true, length: {minimum: 2}
+    validates :author, presence: true
 end
